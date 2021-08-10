44a2a4815b3e022a4dc0270482b823d017a86679
Diego Carvalho <d.carvalho@ieee.org>
2021-08-10 18:22:56 -0300

    fix copiling bug on Pypi

--- Makefile
+++ Makefile
@@ -5,6 +5,9 @@ all: $(MODULE)
 $(MODULE):
 	(cd src; make)
 
+sdev:
+	python3 setup.py sdist
+
 clean:
 	rm -fr $(MODULE) var socnet.egg-info dist build
 	(cd src; make clean)
--- src/pybindmodule.cc
+++ src/pybindmodule.cc
@@ -41,10 +41,52 @@ PYBIND11_MODULE(socnet, m)
 
     m.def("calculate_infection",
           &calculate_infection,
-          "Simulate the Social Network Model for SIRE dynamics.");
+          "Simulate the Social Network Model for SIRE dynamics.\n"
+          "Parameters:\n"
+          " arg0: const int duration,\n"
+          " arg1: const int susceptible_max_size,\n"
+          " arg2: const int i0active,\n"
+          " arg3: const int i0recovered,\n"
+          " arg4: const int samples,\n"
+          " arg5: const int max_transmission_day,\n"
+          " arg6: const int max_in_quarantine,\n"
+          " arg7: const double gamma,\n"
+          " arg8: const double percentage_in_quarantine\n"
+          "Return value: ret = list(list())\n"
+          " ret[0]: infected (mean)\n"
+          " ret[1]: infected (standard deviation)\n"
+          " ret[2]: infected (count per day)\n"
+          " ret[3]: susceptible (mean)\n"
+          " ret[4]: susceptible (standard deviation)\n"
+          " ret[5]: susceptible (count per day)\n"
+          " ret[6]: R0 (mean)\n"
+          " ret[7]: R0 (standard deviation)\n"
+          " ret[8]: R0 (count per day)\n");
 
     m.def(
       "calculate_infection_with_vaccine",
       &calculate_infection_with_vaccine,
-      "Simulate the Social Network Model for SIRE dynamics with vaccination.");
+      "Simulate the Social Network Model for SIRE dynamics with vaccination."
+      "Parameters:\n"
+      " arg0: const int duration,\n"
+      " arg1: const int susceptible_max_size,\n"
+      " arg2: const int i0active,\n"
+      " arg3: const int i0recovered,\n"
+      " arg4: const int samples,\n"
+      " arg5: const int max_transmission_day,\n"
+      " arg6: const int max_in_quarantine,\n"
+      " arg7: const double gamma,\n"
+      " arg8: const double percentage_in_quarantine\n"
+      " arg9: const double vaccinated_share\n"
+      " arg10:const double vaccine_efficacy\n"
+      "Return value: ret = list(list())\n"
+      " ret[0]: infected (mean)\n"
+      " ret[1]: infected (standard deviation)\n"
+      " ret[2]: infected (count per day)\n"
+      " ret[3]: susceptible (mean)\n"
+      " ret[4]: susceptible (standard deviation)\n"
+      " ret[5]: susceptible (count per day)\n"
+      " ret[6]: R0 (mean)\n"
+      " ret[7]: R0 (standard deviation)\n"
+      " ret[8]: R0 (count per day)\n");
 }
